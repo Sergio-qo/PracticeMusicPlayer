f4d8cf4261a5ffa67c7925649bd309a7254087cc
Sergio <sergio.qo000@gmail.com>
2019-05-27 20:47:11 +0200

    Proyect updated, only is needed to solve some bugs and make the code object oriented.

--- PracticeMusicPlayer/app/src/main/java/com/qo/sergio/practicemusicplayer/MainActivity.java
+++ PracticeMusicPlayer/app/src/main/java/com/qo/sergio/practicemusicplayer/MainActivity.java
@@ -4,6 +4,7 @@ package com.qo.sergio.practicemusicplayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.*;
+import android.view.View;
 import android.widget.*;
 
 
@@ -32,5 +33,148 @@ public class MainActivity extends AppCompatActivity {
         mpvector[2] = MediaPlayer.create(this, R.raw.tea);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Play Pause method
+    public void PlayPause(View view)
+    {
+
+        if(mpvector[position].isPlaying())
+        {
+            mpvector[position].pause();
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            mpvector[position].start();
+            play_pause.setBackgroundResource(R.drawable.pausa);
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    //Stop method
+
+    public void Stop(View view)
+    {
+        if(mpvector[position] != null)
+        {
+            mpvector[position].stop();
+            mpvector[0] = MediaPlayer.create(this, R.raw.race);
+            mpvector[1] = MediaPlayer.create(this, R.raw.sound);
+            mpvector[2] = MediaPlayer.create(this, R.raw.tea);
+            position = 0;
+            play_pause.setBackgroundResource(R.drawable.reproducir);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //Repeat method
+
+    public void Repeat(View view)
+    {
+        if(repeat == 1)
+        {
+            btn_repeat.setBackgroundResource(R.drawable.no_repetir);
+            Toast.makeText(this, "No repeat", Toast.LENGTH_SHORT).show();
+            mpvector[position].setLooping(false);
+            repeat = 2;
+        }
+        else
+        {
+            btn_repeat.setBackgroundResource(R.drawable.repetir);
+            Toast.makeText(this, "Repeat", Toast.LENGTH_SHORT).show();
+            mpvector[position].setLooping(true);
+            repeat = 1;
+        }
+    }
+
+    //Next song method
+
+    public void NextSong(View view)
+    {
+        if(position < mpvector.length - 1)
+        {
+            if(mpvector[position].isPlaying())
+            {
+                mpvector[position].stop();
+                position++;
+                mpvector[position].start();
+
+                switch (position)
+                {
+                    case 1:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 3:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+            }
+            else
+            {
+                position++;
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "No more songs", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    //back method
+
+    public void Back(View view)
+    {
+        if(position >= 1)
+        {
+            if(mpvector[position].isPlaying())
+            {
+                mpvector[position].stop();
+                mpvector[0] = MediaPlayer.create(this, R.raw.race);
+                mpvector[1] = MediaPlayer.create(this, R.raw.sound);
+                mpvector[2] = MediaPlayer.create(this, R.raw.tea);
+                position--;
+                switch (position)
+                {
+                    case 1:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 3:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+                mpvector[position].start();
+            }
+            else
+            {
+                switch (position)
+                {
+                    case 1:
+                        iv.setImageResource(R.drawable.portada1);
+                        break;
+                    case 2:
+                        iv.setImageResource(R.drawable.portada2);
+                        break;
+                    case 3:
+                        iv.setImageResource(R.drawable.portada3);
+                        break;
+                }
+                position--;
+            }
+        }
+        else
+        {
+            Toast.makeText(this, "There aren't songs", Toast.LENGTH_SHORT).show();
+        }
     }
 }
